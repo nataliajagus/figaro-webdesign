c04b5d0e9129010519876c7f7681a8ce2b5a2877
Natalia Jaguś <natalia.jagus@gmail.com>
2020-03-27 19:42:43 +0100

    add security section

---
+++ src/components/MobileSection.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components"
+import LeftBg from "../assets/images/bg-left.svg";
+import MobileImg from "../assets/images/mobile.svg";
+
+const SectionWrapper = styled.section`
+  background-image:  url(${LeftBg});
+  background-repeat: no-repeat;
+  background-position: left top;
+  background-size: 50%;
+
+  img {
+    max-width: 550px;
+    width: 100%;
+  }
+
+  h3 {
+    font-size: 2rem;
+  }
+
+  h3, p {
+    max-width: 500px;
+  }
+
+  @media(min-width: 644px) {
+    background-size: 35%;
+  }
+
+  @media(min-width: 992px) {
+    background-size: contain;
+    .w-50:nth-child(2) {
+      padding-left: 60px;
+    }
+  }
+`;
+
+const MobileSection = () => (
+    <SectionWrapper>
+      <div className="container">
+        <div className="row">
+          <div className="w-50">
+            <img src={MobileImg} />
+          </div>
+          <div className="w-50">
+            <h3>Mobilne strony internetowe</h3>
+            <p>Wszystkie strony tworzymy zgodnie z zasadą Responsive Web Design, dzięki czemu skalują się odpowiednio na każdej rozdzielczości i wyświetlają się prawidłowo na komputerach, laptopach, tabletach, telefonach, a nawet na SmartTV.</p>
+            <strong>Dlaczego jest to ważne?</strong>
+            <p>Ponad 80% ruchu w internecine generowana jest przez przeglądarki w urządzeniach mobilnych. Jeśli Twoja strona nie wyświetla się prawidłowo na telefonach, użytkownicy bardzo szybko z niej wyjdą, bo nie będą w stanie łatwo się po niej poruszać.</p>
+          </div>
+        </div>
+      </div>
+    </SectionWrapper>
+)
+
+export default MobileSection;
+
---
+++ src/components/SecuritySection.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components"
+import RightBg from "../assets/images/bg-right.svg";
+import SecurityImg from "../assets/images/security.svg";
+
+const SectionWrapper = styled.section`
+  background-image:  url(${RightBg});
+  background-repeat: no-repeat;
+  background-position: right 20%;
+  background-size: 50%;
+
+  img {
+    max-width: 550px;
+    width: 100%;
+  }
+
+  h3 {
+    font-size: 2rem;
+  }
+
+  h3, p {
+    max-width: 500px;
+  }
+
+  .w-50:first-child {
+    order: 2;
+  }
+
+  .w-50:nth-child(2){
+    order: 1;
+  }
+
+  @media(min-width: 644px) {
+    background-size: 50%;
+  }
+
+  @media(min-width: 768px) {
+    background-position: right center;
+    background-size: 30%;
+    .w-50:first-child {
+    order: 1;
+    }
+
+    .w-50:nth-child(2){
+      order: 2;
+    }
+  }
+
+`;
+
+const SecuritySection = () => (
+    <SectionWrapper>
+      <div className="container">
+        <div className="row">
+          <div className="w-50">
+            <h3>Nowoczesne i bezpieczne</h3>
+            <p>Strony internetowe projektujemy zgodnie z najnowszymi trendami. Każda sekcja i element jest dokładnie przemyślany, tak żeby Twoja strona wyglądała profesjonalnie i wzmacniała pozycję Twojej firmy na rynku. .</p>
+            <p>Dodatkowo do każdej realizowanej strony instalujemy certyfikat SSL, który szyfruje połączenie i pozwala użytkownikom na bezpieczne korzystanie z witryny. Taki certyfikat wpływa też pozytywnie na wyświetlanie się Twojej strony w wynikach Google.</p>
+          </div>
+          <div className="w-50">
+            <img src={SecurityImg} />
+          </div>
+        </div>
+      </div>
+    </SectionWrapper>
+)
+
+export default SecuritySection;
+
